7c4cd2e18588f5b9c873139327f186984b6f2fcb
GulamShahal <117358405+GulamShahal@users.noreply.github.com>
2023-05-26 14:53:18 +0530

    form url update

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,17 +1,17 @@
 $("#submit-form").submit((e)=>{
-  e.preventDefault()
-  $.ajax({
-      url:"https://script.google.com/macros/s/AKfycbwU7_NJDE3O3Az_ZIww3NbQ55aonYTrbc0Ymyj7Nse4iqegItmdv6GAYkM49Yd9SijA/exec",
-      data:$("#submit-form").serialize(),
-      method:"post",
-      success:function (response){
-          alert("Form submitted successfully")
-          window.location.reload()
-          //window.location.href="https://google.com"
-      },
-      error:function (err){
-          alert("Something Error")
-
-      }
+    e.preventDefault()
+    $.ajax({
+        url:"https://script.google.com/macros/s/AKfycbwU7_NJDE3O3Az_ZIww3NbQ55aonYTrbc0Ymyj7Nse4iqegItmdv6GAYkM49Yd9SijA/exec",
+        data:$("#submit-form").serialize(),
+        method:"post",
+        success:function (response){
+            alert("Form submitted successfully")
+            window.location.reload()
+            //window.location.href="https://google.com"
+        },
+        error:function (err){
+            alert("Something Error")
+  
+        }
+    })
   })
-})
